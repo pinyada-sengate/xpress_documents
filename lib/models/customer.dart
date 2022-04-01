99a17f823495f853dd927c2749beae3b9e3656cd
Pizza <pinyadapizza@gmail.com>
2022-04-01 15:06:45 +0700

    add customer ui

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -1,15 +1,41 @@
 class Customer {
+  final String id;
   final String name;
   final String surname;
   final String imageUrl;
+  final String phoneNumber;
+  final String email;
+  final String address;
+  final String city;
+  final String state;
+  final int zipcode;
+
+  //case info
+  final String caseType;
+  final String caseStatus;
+  final String startDate;
   final int price;
   final int paid;
+  final String paidDate;
 
   Customer({
+    required this.id,
     required this.name,
     required this.surname,
     required this.imageUrl,
+    required this.phoneNumber,
+    required this.email,
+    required this.address,
+    required this.city,
+    required this.state,
+    required this.zipcode,
+
+    //case info
+    required this.caseType,
+    required this.caseStatus,
+    required this.startDate,
     required this.price,
     required this.paid,
+    required this.paidDate,
   });
 }
--- lib/screens/add_customer_screen.dart
+++ lib/screens/add_customer_screen.dart
@@ -15,10 +15,212 @@ class _AddCustomerScreenState extends State<AddCustomerScreen> {
         title: const Text('Add New Customer'),
         centerTitle: true,
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Text('Profile'),
-          Text('Name'),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Text(
+              'Personal',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Name',
+                prefixIcon: Icon(
+                  Icons.person_outline,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Surname',
+                prefixIcon: Icon(
+                  Icons.person_outline,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Telephone Number',
+                prefixIcon: Icon(
+                  Icons.phone,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Email',
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Text(
+              'Address',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Address',
+                prefixIcon: Icon(
+                  Icons.home,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'City',
+                prefixIcon: Icon(
+                  Icons.location_city_outlined,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'State',
+                prefixIcon: Icon(
+                  Icons.location_city_outlined,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Zipcode',
+                prefixIcon: Icon(
+                  Icons.local_shipping_outlined,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    )
+                  ),
+                ),
+                child: const Text(
+                  'Next',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
