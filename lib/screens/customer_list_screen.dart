5e74bfbf26bd9fb0acfb72ebe187e3e0de72e8ae
Pizza <pinyadapizza@gmail.com>
2022-04-08 20:49:02 +0700

    show customer list from database

--- lib/screens/customer_list_screen.dart
+++ lib/screens/customer_list_screen.dart
@@ -1,6 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import '../data/data.dart';
 import '../models/customer.dart';
 import '../widgets/search_customer.dart';
 import 'customer_screen.dart';
@@ -13,90 +13,112 @@ class CustomerListScreen extends StatefulWidget {
 }
 
 class _CustomerListScreenState extends State<CustomerListScreen> {
-  _buildCustomers() {
-    List<Widget> customerList = [];
-    for (Customer customer in currentUser.customers) {
-      customerList.add(
-        GestureDetector(
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => CustomerScreen(
-                customer: customer,
-              ),
-            ),
-          ),
-          child: Container(
-            margin:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15.0),
-              border: Border.all(
-                width: 1.0,
-                color: Colors.grey.shade200,
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Image(
-                    width: 100,
-                    height: 100,
-                    image: AssetImage(customer.imageUrl),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(12.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          '${customer.name} ${customer.surname}',
-                          style: const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(
-                          height: 4.0,
-                        ),
-                        Text(
-                          'ID: ${customer.id}',
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(
-                          height: 4.0,
-                        ),
-                        Text(
-                          'Email: ${customer.email}',
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+  Widget _buildCustomer(BuildContext context, Customer customer) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => CustomerScreen(
+            customer: customer,
           ),
         ),
-      );
-    }
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                width: 100,
+                height: 100,
+                image: AssetImage(customer.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '${customer.name} ${customer.surname}',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Phone Number: ${customer.phoneNumber}',
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Email: ${customer.email}',
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-    return Column(children: customerList);
+  _buildCustomerList() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('customers').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+
+        if (!snapshot.hasData) {
+          return const Text('No data');
+        } else {
+          return Expanded(
+            child: ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                Customer customer = Customer.fromJson(data);
+                return _buildCustomer(context, customer);
+              }).toList(),
+            ),
+          );
+        }
+      },
+    );
   }
 
   @override
@@ -106,26 +128,22 @@ class _CustomerListScreenState extends State<CustomerListScreen> {
         title: const Text('List of Customers'),
         centerTitle: true,
       ),
-      body: ListView(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           const SearchCustomer(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  'Customers',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.2,
-                  ),
-                ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              'Customers',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.2,
               ),
-              _buildCustomers(),
-            ],
+            ),
           ),
+          _buildCustomerList(),
         ],
       ),
     );
