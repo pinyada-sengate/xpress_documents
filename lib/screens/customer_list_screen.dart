a2e09b9df1b9564aedf1b81cb5ba5901a0868d07
Pizza <pinyadapizza@gmail.com>
2022-04-08 16:42:59 +0700

    customer list ui

--- lib/screens/customer_list_screen.dart
+++ lib/screens/customer_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../data/data.dart';
+import '../models/customer.dart';
 import '../widgets/search_customer.dart';
+import 'customer_screen.dart';
 
 class CustomerListScreen extends StatefulWidget {
   const CustomerListScreen({Key? key}) : super(key: key);
@@ -10,6 +13,92 @@ class CustomerListScreen extends StatefulWidget {
 }
 
 class _CustomerListScreenState extends State<CustomerListScreen> {
+  _buildCustomers() {
+    List<Widget> customerList = [];
+    for (Customer customer in currentUser.customers) {
+      customerList.add(
+        GestureDetector(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => CustomerScreen(
+                customer: customer,
+              ),
+            ),
+          ),
+          child: Container(
+            margin:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(
+                width: 1.0,
+                color: Colors.grey.shade200,
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    width: 100,
+                    height: 100,
+                    image: AssetImage(customer.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${customer.name} ${customer.surname}',
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          'ID: ${customer.id}',
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          'Email: ${customer.email}',
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(children: customerList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +107,25 @@ class _CustomerListScreenState extends State<CustomerListScreen> {
         centerTitle: true,
       ),
       body: ListView(
-        children: const <Widget>[
-          SearchCustomer(),
+        children: <Widget>[
+          const SearchCustomer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Customers',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ),
+              _buildCustomers(),
+            ],
+          ),
         ],
       ),
     );
