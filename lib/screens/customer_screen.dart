617fba9045ed1fa5735734df722598346fc5455a
Pizza <pinyadapizza@gmail.com>
2022-04-16 18:40:09 +0700

    customer profile ui

--- lib/screens/customer_screen.dart
+++ lib/screens/customer_screen.dart
@@ -5,7 +5,7 @@ import '../models/customer.dart';
 class CustomerScreen extends StatefulWidget {
   final Customer customer;
 
-  const CustomerScreen({ Key? key, required this.customer }) : super(key: key);
+  const CustomerScreen({Key? key, required this.customer}) : super(key: key);
 
   @override
   State<CustomerScreen> createState() => _CustomerScreenState();
@@ -19,9 +19,272 @@ class _CustomerScreenState extends State<CustomerScreen> {
         title: Text('${widget.customer.name} ${widget.customer.surname}'),
         centerTitle: true,
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Text('${widget.customer.name} ${widget.customer.surname} Profile'),
+          Container(
+            margin:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    width: 100,
+                    height: 100,
+                    image: AssetImage(widget.customer.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${widget.customer.name} ${widget.customer.surname}',
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              phoneNumberField(),
+              emailField(),
+              addressField(),
+              cityField(),
+              stateField(),
+              zipCodeField(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget phoneNumberField() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'Phone Number : ',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          Expanded(
+            child: Text(
+              widget.customer.phoneNumber,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'Email : ',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          Expanded(
+            child: Text(
+              widget.customer.email,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget addressField() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'Address : ',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          Expanded(
+            child: Text(
+              widget.customer.address,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget cityField() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'City : ',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          Expanded(
+            child: Text(
+              widget.customer.city,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget stateField() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'State : ',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          Expanded(
+            child: Text(
+              widget.customer.state,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget zipCodeField() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'Zip Code : ',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          Expanded(
+            child: Text(
+              widget.customer.zipCode,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
         ],
       ),
     );
